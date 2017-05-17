7aca0e1cf828e4aff60d33fe7a471820f2363974
ste9206 <stefano.gardano@gmail.com>
2017-05-17 16:56:27 +0200

    first project on iOS using pickerView

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    let states = ["Piemonte","Lombardia","Veneto","Liguria","Sardegna"]
+    
+
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
+    
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func btnAction(_ sender: Any) {
+         statePicker.isHidden = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerTextField.text = states[row]
+        statePicker.isHidden = true
+    }
 
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
 }
 
